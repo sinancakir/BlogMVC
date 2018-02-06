95ea8cd7d893c3c16fe56f177636075a6d821211
sinancakir <sinanckrr@gmail.com>
2018-02-06 14:22:40 +0300

    first commit

---
+++ BlogMvcApp/Controllers/CategoryController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BlogMvcApp.Models;
+
+namespace BlogMvcApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private BlogContext db = new BlogContext();
+        
+
+        public PartialViewResult KategoriListesi()
+        {
+            //int blogSayisi = db.Kategoriler.Count();
+            //ViewBag.blogSayisi = blogSayisi;
+            var kategoriler = db.Kategoriler
+                                .Select(i =>
+                                    new CategoryModel()
+                                    {
+                                        Id = i.Id,
+                                        KategoriAdi = i.KategoriAdi,
+                                        BlogSayisi = i.Bloglar.Count()
+                                    }
+                                );
+            return PartialView(kategoriler.ToList());
+        }
+
+        // GET: Category
+        public ActionResult Index()
+        {
+            var kategoriler = db.Kategoriler
+                                .Select(i =>
+                                    new CategoryModel()
+                                    {
+                                        Id = i.Id,
+                                        KategoriAdi = i.KategoriAdi,
+                                        BlogSayisi = i.Bloglar.Count()
+                                    }
+                                );
+
+            return View(kategoriler.ToList());
+        }
+
+        // GET: Category/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Kategoriler.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "KategoriAdi")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Kategoriler.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(category);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Kategoriler.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,KategoriAdi")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+
+                TempData["Kategori"] = category;
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Kategoriler.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Category category = db.Kategoriler.Find(id);
+            db.Kategoriler.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
